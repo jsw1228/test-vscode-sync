8d6d364e8cd4eeb37b29118705278d67896c294c
jsw1228 <jakew1228@gmail.com>
2025-10-23 14:36:08 -0400

    comitting new file

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ st.title("Wassuupppieee")
 st.write("this is my first streamlittiee app from vs code")
 
 st.write("im adding this line to see if it works!!!")
+
+
+st.write("im adding erkjqe;rkfnq;efthis line to see if it works!!!")
